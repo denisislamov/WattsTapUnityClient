3090fc379f8cf50b9104d2f6761c4ebe1d0a5bc4
denisislamov <islamov.denis@gmail.com>
2025-10-22 23:52:35 +0800

    Add MainMenu UI, TextMesh Pro, WebGL support and Bootstrap scene

---
+++ Assets/WattsTap/Scripts/Constants/UIConstants.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace WattsTap.Constants
+{
+    public static class UIConstants
+    {
+        public const string MainMenu = "MainMenu";
+    }
+}
---
+++ Assets/WattsTap/Scripts/Game/UI/MainMenuUIView.cs
@@ -0,0 +1,9 @@
+using WattsTap.Core.UI;
+
+namespace WattsTap.Game.UI
+{
+    public class MainMenuUIView : UIBaseView<MainMenuUIPresenter>
+    {
+        
+    }
+}
