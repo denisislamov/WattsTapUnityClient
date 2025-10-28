6ec33a17138a94391017f5e953849bcade3507b0
denisislamov <islamov.denis@gmail.com>
2025-10-28 23:53:27 +0800

    Add ui model to UI MVP with sample as Main Menu

---
+++ Assets/WattsTap/Scripts/Core/UI/UIBaseModel.cs
@@ -0,0 +1,14 @@
+namespace WattsTap.Core.UI
+{
+    public abstract class UIBaseModel
+    {
+        public virtual void Initialize()
+        {
+        }
+
+        public virtual void Dispose()
+        {
+        }
+    }
+}
+
--- Assets/WattsTap/Scripts/Core/UI/UIBasePresenter.cs
+++ Assets/WattsTap/Scripts/Core/UI/UIBasePresenter.cs
@@ -1,16 +1,28 @@
 namespace WattsTap.Core.UI
 {
-    public abstract class UIBasePresenter<TView> : IUIViewPresenter where TView : IUIView
+    public abstract class UIBasePresenter<TView, TModel> : IUIViewPresenter 
+        where TView : IUIView
+        where TModel : UIBaseModel, new()
     {
         protected TView View;
+        protected TModel Model;
 
         public void Initialize(IUIView view)
         {
             View = (TView)view;
+            Model = new TModel();
+            Model.Initialize();
             OnInit();
         }
 
         protected abstract void OnInit();
-        public abstract void Dispose();
+        
+        public virtual void Dispose()
+        {
+            Model?.Dispose();
+            OnDispose();
+        }
+
+        protected abstract void OnDispose();
     }
 }
