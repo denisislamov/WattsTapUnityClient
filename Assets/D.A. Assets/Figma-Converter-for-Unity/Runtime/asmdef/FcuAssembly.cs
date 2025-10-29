9b6614d0e354133e7dd717c44a24fd01c974cb92
denisislamov <islamov.denis@gmail.com>
2025-10-29 22:02:08 +0800

    add figma importer and main menu scene

---
+++ Assets/D.A. Assets/Figma-Converter-for-Unity/Runtime/asmdef/FcuAssembly.cs
@@ -0,0 +1,9 @@
+namespace DA_Assets.FCU
+{
+    public class FcuAssembly
+    {
+        /// That empty assembly is legacy — you can ignore it.
+        /// The asset files weren't placed into that assembly because
+        /// in certain scenarios it caused errors in Unity 2021 and 2022.
+    }
+}
