16e9fee5f9555ceba410895dabed30854b495620
denisislamov <islamov.denis@gmail.com>
2026-01-02 23:27:08 +0800

    add shop booster item

--- Assets/WattsTap/Scripts/Game/UI/ShopBoosterItemUIView.cs
+++ Assets/WattsTap/Scripts/Game/UI/ShopBoosterItemUIView.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using WattsTap.Core;
 using WattsTap.Core.UI;
 using WattsTap.Game.Shop;
 
@@ -15,6 +16,11 @@ namespace WattsTap.Game.UI
 
         [Header("Controls")]
         [SerializeField] private Button _backButton;
+
+        [Header("Skinning")]
+        [SerializeField] private MainMenuThemeManager.SkinTokenBinding[] _skinBindings;
+        
+        private MainMenuThemeManager _themeManager;
 
         public Button BackButton => _backButton;
 
@@ -51,6 +57,40 @@ namespace WattsTap.Game.UI
             if (_descriptionText != null)
             {
                 _descriptionText.text = description;
+            }
+        }
+        
+        private void OnEnable()
+        {
+            _themeManager = ServiceLocator.Get<MainMenuThemeManager>();
+            
+            if (_themeManager != null)
+            {
+                _themeManager.SkinChanged += OnSkinChanged;
+
+                if (_themeManager.CurrentSkin != null)
+                {
+                    _themeManager.ApplySkin(_themeManager.CurrentSkin, _skinBindings, this);
+                    return;
+                }
+                
+                _themeManager.ApplySkin(null, _skinBindings, this);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (_themeManager != null)
+            {
+                _themeManager.SkinChanged -= OnSkinChanged;
+            }
+        }
+        
+        private void OnSkinChanged(MainMenuSkinDefinition skin)
+        {
+            if (_themeManager != null)
+            {
+                _themeManager.ApplySkin(skin, _skinBindings, this);
             }
         }
     }
